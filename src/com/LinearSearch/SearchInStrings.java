b1c5cbe67ee0876cfc5f139f4387aae394306a32
Samriddhi <samriddhi.2125it1146@kiet.edu>
2023-10-16 21:50:27 +0530

    New File was created

---
+++ src/com/LinearSearch/SearchInStrings.java
@@ -0,0 +1,22 @@
+package com.LinearSearch;
+
+public class SearchInStrings {
+    public static void main(String[] args) {
+        String name = "Samriddhi";
+        char target = 'd';
+        System.out.println(search(name, target));
+    }
+    static boolean search(String str, char target) {
+        if (str.length() == 0) {
+            return false;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if (target == str.charAt(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+
