8c197695a3a813efa85e720a06ae4c315c519604
Samriddhi <samriddhi.2125it1146@kiet.edu>
2023-10-29 14:06:44 +0530

    New File was created

---
+++ src/com/BinarySearch/Binary_Search.java
@@ -0,0 +1,32 @@
+package com.BinarySearch;
+
+public class Binary_Search {
+    public static void main(String[] args) {
+        int[] arr = {-18, -12, -4, 3, 4, 15, 22, 45, 67, 98};
+        int target = 22;
+        int ans = binarySearch(arr, target);
+        System.out.println(ans);
+    }
+    static int binarySearch(int[] arr, int target)
+    {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end) {
+            //find the middle element
+           // int mid = (start+end)/2;  //might be possible that (start+end) exceeds the range of integer in java
+            int mid  = start + (end - start) / 2;
+
+            if ( target < arr[mid]) {
+                end  = mid - 1;
+            }
+            else if (target > arr[mid]) {
+                start = mid + 1;
+            }
+            else {
+                 return mid;
+            }
+        }
+        return -1;
+    }
+}
