e9a572831b66d7ae7e759c1585cfa7fbbf6494b0
Samriddhi <samriddhi.2125it1146@kiet.edu>
2023-10-29 14:08:06 +0530

    New File was created

--- src/com/BinarySearch/Ceiling.java
+++ src/com/BinarySearch/Ceiling.java
@@ -20,7 +20,7 @@ public class Ceiling {
             if (target < arr[mid]) {
                 end = mid - 1;
             } else if (target > arr[mid]) {
-                start = mid + 1;
+                start = mid +  1;
             } else {
                 return mid;
             }
