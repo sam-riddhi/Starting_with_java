0174e716396afc59e4645be8b070a87a27dd1295
Samriddhi <samriddhi.2125it1146@kiet.edu>
2024-02-13 23:31:07 +0530

    New File was created

---
+++ src/com/Strings/Main.java
@@ -0,0 +1,15 @@
+package com.Strings;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 4, 19};
+        int num = 10;
+        String name = "Samriddhi";
+//        System.out.println(name);
+
+        String a = "Samriddhi";
+        System.out.println(a);
+        a = "Jaiswal";
+        System.out.println(a);
+    }
+}
