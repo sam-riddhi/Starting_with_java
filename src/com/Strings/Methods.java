b58f5c202088d8be8e0b75a32ab6964e76e05385
Samriddhi <samriddhi.2125it1146@kiet.edu>
2024-02-13 23:59:26 +0530

    New File was created

--- src/com/Strings/Methods.java
+++ src/com/Strings/Methods.java
@@ -2,12 +2,12 @@ package com.Strings;
 import java.util.Arrays;
 public class Methods {
     public static void main(String[] args) {
-        String name = "Kunal Kushwaha Hello World";
+        String name = "Samriddhi Jaiswal Hello World";
         System.out.println(Arrays.toString(name.toCharArray()));
         System.out.println(name.toLowerCase());
         System.out.println(name);
         System.out.println(name.indexOf('a'));
-        System.out.println("     Kunal   ".strip());
+        System.out.println("     Samriddhi   ".strip());
         System.out.println(Arrays.toString(name.split(" ")));
     }
 }
