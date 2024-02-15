75ef1044e748b7da8d16f72a061bd39ad2b68332
Samriddhi <samriddhi.2125it1146@kiet.edu>
2024-02-16 00:05:32 +0530

    New File was created

---
+++ src/com/Pattern/Pattern2.java
@@ -0,0 +1,18 @@
+package com.Pattern;
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        pattern2(4);
+
+    }
+    static void pattern2(int n) {
+        for (int row = 1; row <= n; row++)
+        { // for every row , run a col
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            //when one row is printed we need to add a newline
+            System.out.println();
+        }
+    }
+}
