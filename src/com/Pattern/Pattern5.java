1c4f8a56c4da35d9c419ac1b6c50d13bb88638e8
Samriddhi <samriddhi.2125it1146@kiet.edu>
2024-02-16 13:18:41 +0530

    New File was created

---
+++ src/com/Pattern/Pattern5.java
@@ -0,0 +1,29 @@
+package com.Pattern;
+/*
+    *
+    * *
+    * * *
+    * * * *
+    * * * * *
+    * * * *
+    * * *
+    * *
+    *
+ */
+public class Pattern5 {
+    public static void main(String[] args) {
+        pattern5(5);
+    }
+    static void pattern5(int n)
+    {
+        for (int row = 1; row < 2*n; row++)
+        {
+            int totalColsInRow = row > n ? 2 * n - row : row;
+            for(int col = 0; col < totalColsInRow; col++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
