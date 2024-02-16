9dcca6bf2a3f9b4ee69c738e48f7193dc58e12ab
Samriddhi <samriddhi.2125it1146@kiet.edu>
2024-02-16 19:53:58 +0530

    New File was created

---
+++ src/com/Pattern/Pattern30.java
@@ -0,0 +1,32 @@
+package com.Pattern;
+/*
+            1
+          2 1 2
+        3 2 1 2 3
+      4 3 2 1 2 3 4
+    5 4 3 2 1 2 3 4 5
+ */
+public class Pattern30 {
+    public static void main(String[] args) {
+        pattern30(5);
+    }
+    static void pattern30(int n)
+    {
+        for (int row = 0; row < n; row++)
+        {
+            for (int space = 0; space <= n-row; space++)
+            {
+                System.out.print(" ");
+            }
+            for (int col = row; col >= 1; col--)
+            {
+                System.out.print(col);
+            }
+            for (int col = 2; col <= row; col++)
+            {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
