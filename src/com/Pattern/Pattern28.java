94d49379490f4b3416931485e8ae9fa008ed41c7
Samriddhi <samriddhi.2125it1146@kiet.edu>
2024-02-16 19:11:13 +0530

    New File was created

---
+++ src/com/Pattern/Pattern28.java
@@ -0,0 +1,24 @@
+package com.Pattern;
+
+public class Pattern28 {
+    public static void main(String[] args) {
+        pattern28(5);
+    }
+    static void pattern28(int n)
+    {
+        for (int row = 0; row < 2 * n; row++)
+        {
+            int totalColsInRow = row > n ? 2 * n - row: row;
+            int noOfSpaces = n - totalColsInRow;
+            for(int s = 0; s < noOfSpaces; s++ )
+            {
+                System.out.print(" ");
+            }
+            for (int col = 0; col < totalColsInRow; col++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
