d0b8c3560ca104fc662555cbfb72b6ed25e2c7a0
Samriddhi <samriddhi.2125it1146@kiet.edu>
2024-02-18 01:39:49 +0530

    New File was created

---
+++ src/com/Recursion/NumbersExample.java
@@ -0,0 +1,30 @@
+package com.Recursion;
+
+public class NumbersExample {
+    public static void main(String[] args) {
+        //write a function that takes in a number and prints it
+        //print 1st 5 numbers
+        print1(1);
+
+    }
+
+    static void print1(int n){
+        System.out.println(n);
+        print2(2);
+    }
+    static void print2(int n){
+        System.out.println(n);
+        print3(3);
+    }
+    static void print3(int n){
+        System.out.println(n);
+        print4(4);
+    }
+    static void print4(int n){
+        System.out.println(n);
+        print5(5);
+    }
+    static void print5(int n){
+        System.out.println(n);
+    }
+}
