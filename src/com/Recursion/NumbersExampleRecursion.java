656b8618b0ed01231c08c686aefe7dd331585420
Samriddhi <samriddhi.2125it1146@kiet.edu>
2024-02-18 13:08:32 +0530

    New File was created

--- src/com/Recursion/NumbersExampleRecursion.java
+++ src/com/Recursion/NumbersExampleRecursion.java
@@ -15,6 +15,9 @@ public class NumbersExampleRecursion {
 
         // Recursive call
         // If you are calling a function again and again,  you can treat it as a separate call in the stack
+
+        // this is called tail recursion
+        // this is the last function call
         print(n+1);
     }
 }
