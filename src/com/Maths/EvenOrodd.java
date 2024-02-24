77e0aee257d6d54199a5925ac06f13216e3d39a4
Samriddhi <samriddhi.2125it1146@kiet.edu>
2024-02-25 00:49:01 +0530

    New File was created

---
+++ src/com/Maths/EvenOrodd.java
@@ -0,0 +1,12 @@
+package com.Maths;
+
+public class EvenOrodd {
+    public static void main(String[] args) {
+        int n = 67;
+        System.out.println(isOdd(n));
+    }
+    private static boolean isOdd(int n)
+    {
+        return (n & 1) == 1;
+    }
+}
