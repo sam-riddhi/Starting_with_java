f30c3b966aa0974cce644312b93adebc9da3114b
Samriddhi <samriddhi.2125it1146@kiet.edu>
2024-02-24 11:19:49 +0530

    New File was created

--- src/com/Recursion/Fibo.java
+++ src/com/Recursion/Fibo.java
@@ -2,9 +2,21 @@ package com.Recursion;
 
 public class Fibo {
     public static void main(String[] args) {
-        int ans  = fibo(6);
-        System.out.println(ans);
+//        int ans  = fibo(6);
+//        System.out.println(ans);
+
+        for (int i = 0; i < 11; i++)
+        {
+            System.out.println(fiboFormula(i));
+        }
+        System.out.println(fiboFormula(50));
     }
+
+    static int fiboFormula(int n) {
+        // just for demo , use long instead
+        return (int)(Math.pow(((1 + Math.sqrt(5)) / 2), n) / Math.sqrt(5));
+    }
+
     static int fibo(int n)
     {
         //base condition
