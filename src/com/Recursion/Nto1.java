dba7202ffe65b35ee416e8144a7ee5bfa32c2b88
Samriddhi <samriddhi.2125it1146@kiet.edu>
2024-02-27 22:20:21 +0530

    New File was created

--- src/com/Recursion/Nto1.java
+++ src/com/Recursion/Nto1.java
@@ -2,7 +2,7 @@ package com.Recursion;
 
 public class Nto1 {
     public static void main(String[] args) {
-        funcRev(5);
+        funcBoth(5);
     }
 
     static void func(int n)
@@ -22,6 +22,16 @@ public class Nto1 {
             return;
         }
         funcRev(n-1);
+        System.out.println(n);
+    }
+    static void funcBoth(int n)
+    {
+        if (n == 0)
+        {
+            return;
+        }
+        System.out.println(n);
+        funcBoth(n-1);
         System.out.println(n);
     }
 }
