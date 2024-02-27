05f55bcf6ce4ab76830bca68bd9960e717f222cc
Samriddhi <samriddhi.2125it1146@kiet.edu>
2024-02-27 12:21:22 +0530

    New File was created

---
+++ src/com/Maths/Factors.java
@@ -0,0 +1,52 @@
+package com.Maths;
+
+import java.util.ArrayList;
+
+public class Factors {
+    public static void main(String[] args) {
+        factors3(20);
+    }
+    //O(N)
+    static void factors(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                System.out.print(i + " ");
+            }
+        }
+    }
+    static void factors2(int n) {
+        for (int i = 1; i <= Math.sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                if (n / i == i)
+                {
+                    System.out.print(i + " ");
+                }
+                else
+                {
+                    System.out.print(i + " " + n / i + " ");
+                }
+            }
+        }
+    }
+    static void factors3(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                if (n / i == i) {
+                    System.out.print(i + " ");
+                } else {
+                    System.out.print(i + " ");
+                    list.add(n / i);
+                }
+            }
+        }
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.out.print(list.get(i) + " ");
+        }
+    }
+}
