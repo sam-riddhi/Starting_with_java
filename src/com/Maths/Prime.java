e2466bb4639d5c1fc504b09dcd033794cbcaf7c6
Samriddhi <samriddhi.2125it1146@kiet.edu>
2024-02-27 12:21:52 +0530

    New File was created

---
+++ src/com/Maths/Prime.java
@@ -0,0 +1,28 @@
+package com.Maths;
+
+public class Prime {
+    public static void main(String[] args) {
+        int n = 20;
+        for (int i = 1; i <= n; i++)
+        {
+            System.out.println(i + " " + isprime(i));
+        }
+    }
+    static boolean isprime(int n)
+    {
+        if (n <= 1)
+        {
+            return false;
+        }
+        int c = 2;
+        while (c * c <= n)
+        {
+            if (n % c == 0)
+            {
+                return false;
+            }
+            c++;
+        }
+        return true;
+    }
+}
