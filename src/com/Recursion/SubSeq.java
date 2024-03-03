4aaa15ed5d9e9dcb5b87d378b33ac5ba1702e967
Samriddhi <samriddhi.2125it1146@kiet.edu>
2024-03-03 22:50:43 +0530

    New File was created

---
+++ src/com/Recursion/SubSeq.java
@@ -0,0 +1,41 @@
+package com.Recursion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SubSeq {
+    public static void main(String[] args) {
+        //subseq("", "abc");
+        System.out.println(subseqRet("", "abc"));
+    }
+
+    static void subseq(String p, String up)
+    {
+        if (up.isEmpty())
+        {
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+
+        subseq(p + ch, up.substring(1));
+        subseq(p, up.substring(1));
+    }
+
+    static ArrayList<String> subseqRet(String p, String up)
+    {
+        if (up.isEmpty())
+        {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+
+        ArrayList<String> left = subseqRet(p + ch, up.substring(1));
+        ArrayList<String> right = subseqRet(p, up.substring(1));
+
+        left.addAll(right);
+        return left ;
+    }
+}
