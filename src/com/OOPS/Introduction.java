b1db8549fb4f7fe41a27e93af05c22a4cfaf5231
Samriddhi <samriddhi.2125it1146@kiet.edu>
2024-03-07 23:39:07 +0530

    New File was created

---
+++ src/com/OOPS/Introduction.java
@@ -0,0 +1,29 @@
+package com.OOPS;
+
+public class Introduction {
+    public static void main(String[] args) {
+        // stores 5 roll nos
+        int [] numbers = new int [5];
+
+        // stores 5 names
+        String[] names = new String[5];
+
+        // data of 5 students: {roll no, name, marks}
+        int[] rno = new int[5];
+        String[] name = new String[5];
+        float[] marks = new float[5];
+
+        // A class is a named group of properties and functions
+
+        Student[] students = new Student[5];
+        Student Sam;
+    }
+
+    // create a class
+    class Student
+    {
+        int[] rno = new int[5];
+        String[] name = new String[5];
+        float[] marks = new float[5];
+    }
+}
