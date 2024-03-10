008827fd46a6ed342cf26365d8f6ef638e0ba514
Samriddhi <samriddhi.2125it1146@kiet.edu>
2024-03-10 23:53:15 +0530

    New File was created

---
+++ src/com/OOPS/packages/b/Message.java
@@ -0,0 +1,12 @@
+package com.OOPS.packages.b;
+
+public class Message {
+    public static void main(String[] args) {
+
+    }
+
+    public static void message()
+    {
+        System.out.println("This course is awesome ");
+    }
+}
