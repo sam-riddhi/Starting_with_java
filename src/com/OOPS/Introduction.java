7d91caa655b50e1b2d14e5c952ef57b7d8387958
Samriddhi <samriddhi.2125it1146@kiet.edu>
2024-03-10 12:10:17 +0530

    New File was created

--- src/com/OOPS/Introduction.java
+++ src/com/OOPS/Introduction.java
@@ -1,6 +1,7 @@
 package com.OOPS;
 
 import java.util.Arrays;
+import java.util.SimpleTimeZone;
 
 public class Introduction {
     public static void main(String[] args) {
@@ -36,6 +37,19 @@ public class Introduction {
         System.out.println(sam.name);
         System.out.println(sam.marks);
         //System.out.println(Arrays.toString(students));
+
+        Student random = new Student(sam);
+        System.out.println(random.name);
+
+        Student random2 = new Student();
+        System.out.println(random2.name);
+
+        Student one  = new Student();
+        Student two = one;
+
+        one.name = "Something";
+
+        System.out.println(two.name);
     }
 }
     // create a class
@@ -57,13 +71,30 @@ public class Introduction {
 
         void changeName(String newname)
         {
+
             name  = newname;
         }
-        Student(int roll, String name, float marks)
+
+        Student (Student other)
         {
-            rno = roll;
-            name = name;
-            marks = marks;
+            this.name = other.name;
+            this.rno = other.rno;
+            this.marks = other.marks;
+        }
+        Student()
+        {
+            // this is how you call a constructor from another constructor
+            // internally : new Student();
+            this (13,"default person", 100.6f);
+        }
+
+        // Student arpit = new Student(17, "Arpit", 78.9f);
+        // here, this will be replaced with arpit
+        Student(int rno, String name, float marks)
+        {
+            this.rno = rno;
+            this.name = name;
+            this.marks = marks;
         }
     }
 
