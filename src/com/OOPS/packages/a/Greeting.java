991bb3ff48a17c614c9bf421f1de388810e6068f
Samriddhi <samriddhi.2125it1146@kiet.edu>
2024-03-10 23:52:18 +0530

    New File was created

---
+++ src/com/OOPS/packages/a/Greeting.java
@@ -0,0 +1,10 @@
+package com.OOPS.packages.a;
+
+import static com.OOPS.packages.b.Message.message;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        message();
+    }
+}
