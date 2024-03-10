2a55fe8eba984885154ed9953cf4aa2f65caeceb
Samriddhi <samriddhi.2125it1146@kiet.edu>
2024-03-10 23:53:29 +0530

    New File was created

---
+++ src/com/OOPS/StaticExample/StaticBlock.java
@@ -0,0 +1,27 @@
+package com.OOPS.StaticExample;
+
+// this is a demo to show initialization of static variables
+public class StaticBlock {
+    static int a = 4;
+    static int b;
+
+    // static block will only run once, when the first obj is created
+    // i.e. when the class is loaded for the first time
+    static
+    {
+        System.out.println("I am in static block");
+        b = a * 5;
+    }
+
+    public static void main(String[] args) {
+        StaticBlock obj = new StaticBlock();
+        System.out.println(StaticBlock.a + " " + StaticBlock.b);
+
+        StaticBlock.b += 3;
+
+        System.out.println(StaticBlock.a + " " + StaticBlock.b);
+
+        StaticBlock obj2 = new StaticBlock();
+        System.out.println(StaticBlock.a + " " + StaticBlock.b);
+    }
+}
