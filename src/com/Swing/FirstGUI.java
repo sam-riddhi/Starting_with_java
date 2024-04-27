bcd20e825c9f9705197f94e3bfc66d357b1f506f
Samriddhi <samriddhi.2125it1146@kiet.edu>
2024-04-27 14:38:40 +0530

    New File was created

--- src/com/Swing/FirstGUI.java
+++ src/com/Swing/FirstGUI.java
@@ -1,5 +1,6 @@
 package com.Swing;
 import javax.swing.*;
+import java.awt.*;
 
 public class FirstGUI
 {
@@ -10,15 +11,27 @@ public class FirstGUI
     }
 }
 
-class Abc extends JFrame
+class Abc extends JFrame  //CardLayout - JFrame follows card layout that is like a stack of cards , it only shows the card which is at the top, so when 2 texts are there it shows only l1
+    //In order to solve this problem we change the layout by using setLayout
 {
     public Abc()
     {
-        JLabel l = new JLabel("Hello World");
-        add(l);
 
+        JLabel l = new JLabel("Hello World");
+        JLabel l1 = new JLabel("Welcome Sam");
+
+        add(l);
+        add(l1);
+
+        // For every code the below 4 statements are must
+
+        setLayout(new FlowLayout());  //FlowLayout, GridLayout, NullLayout
         setVisible(true); // by default JFrame is not visible when it is created, so setting it true helps it to be visible
-        setSize(400, 400); // sets the six=ze of the frame
+        setSize(400, 400); // sets the size of the frame
+        //On clicking exit button in the frame it doesn't the prog still runs
+        //which means that the exit button is not working properly
+        //to make it work properly we use setDefaultCloseOperations
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //The number for exit on close is 3. So instead of writing exit on close we can 3 also
     }
 }
 
