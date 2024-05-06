7841ab4aa97003d78d61537335cc3ce5a94782aa
Samriddhi <samriddhi.2125it1146@kiet.edu>
2024-05-06 11:54:09 +0530

    New File was created

--- src/com/LinkedList/DoublyLinkedList.java
+++ src/com/LinkedList/DoublyLinkedList.java
@@ -2,10 +2,10 @@ package com.LinkedList;
 
 class Nodee {
     int data;
-    Node_ next;
-    Node_ back;
+    Nodee_ next;
+    Nodee_ back;
 
-    Nodee(int data1, Node_ next1, Node_ back1)
+    Nodee(int data1, Nodee_ next1, Nodee_ back1)
     {
         this.data = data1;
         this.next = next1;
@@ -20,7 +20,7 @@ class Nodee {
     }
 }
 public class DoublyLinkedList {
-    public static void print(Node_ head)
+    public static void print(Nodee_ head)
     {
         while(head != null)
         {
@@ -29,13 +29,13 @@ public class DoublyLinkedList {
         }
         System.out.println();
     }
-    private static Node_ convertArr2Dll(int[] arr)
+    private static Nodee_ convertArr2Dll(int[] arr)
     {
-        Node_ head = new Node_(arr[0]);
-        Node_ prev = head;
+        Nodee_ head = new Nodee_(arr[0]);
+        Nodee_ prev = head;
         for(int i = 1; i < arr.length; i++)
         {
-            Node_ temp = new Node_(arr[i], null, prev);
+            Nodee_ temp = new Nodee_(arr[i], null, prev);
             prev.next = temp;
             prev = temp;
         }
@@ -43,7 +43,7 @@ public class DoublyLinkedList {
     }
     public static void main(String[] args) {
         int[] arr = {12, 5, 6, 8};
-        Node_ head = convertArr2Dll(arr);
+        Nodee_ head = convertArr2Dll(arr);
         print(head);
 
     }
